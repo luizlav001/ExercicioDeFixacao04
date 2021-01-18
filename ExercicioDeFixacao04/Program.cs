3e9bb4f561e2618228d52f99db48b1c6f52c3493
Lincoln doVale <lincolndovale07@gmail.com>
2021-01-18 12:15:21 -0300

    Adicionar arquivos de projeto.

---
+++ ExercicioDeFixacao04/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Globalization;
+
+namespace ExercicioDeFixacao04
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Qual é a cotação do dólar? ");
+            double cotacao = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.Write("Quantos dólares você vai comprar? ");
+            double quantia = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            double resut = ConversorDeMoedas.DolarParaReal(quantia, cotacao);
+
+            Console.WriteLine("Valor a ser pago em reais = " + resut.ToString("F2", CultureInfo.InvariantCulture));
+        }
+    }
+}
